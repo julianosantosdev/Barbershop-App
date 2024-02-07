d7318c9b06670d9c875c9c90785e27958a15363c
Juliano Santos <julianosantosdev@outlook.com>
2024-02-07 19:04:37 -0300

    feat: barber cards, footer, prisma client connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,11 @@ import Header from "@/components/common/header";
 import SearchBar from "@/components/home/searchbar";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import db from "../../prisma/client";
+import BarbershopItem from "@/components/home/barbershop-item";
 
-export default function Home() {
+export default async function Home() {
+  const barbershopList = await db.barbershop.findMany();
   return (
     <>
       <Header />
@@ -27,6 +30,30 @@ export default function Home() {
             Agendamentos
           </h2>
           <BookingItem />
+        </section>
+
+        <section className="mt-6">
+          <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">
+            Recomendados
+          </h2>
+
+          <div className="px-5 flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+            {barbershopList.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershopData={barbershop} />
+            ))}
+          </div>
+        </section>
+
+        <section className="mt-6">
+          <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">
+            Populares
+          </h2>
+
+          <div className="px-5 flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden mb-20">
+            {barbershopList.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershopData={barbershop} />
+            ))}
+          </div>
         </section>
       </main>
     </>
---
+++ src/components/common/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="w-full bg-secondary px-5 py-6">
+      <p className="text-gray-400 mb-2">© 2024 Copyright Barbershop App </p>
+      <small className="text-gray-500">Made by Juliano with ♥ </small>
+    </footer>
+  );
+};
+
+export default Footer;
